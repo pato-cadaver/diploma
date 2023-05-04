49b01504132fe324af512d7c9b9fc78cb4b58475
Andrew Turpanov <catchaglimpse@mail.ru>
2023-05-05 02:04:51 +0800

    create react app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import logo from 'C:/06690/diploma/src/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Text
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
